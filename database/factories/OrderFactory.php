43869232b3e7b5f62ba5444345f63f08e7e37b5b
Carlos Junior <carlos@theplace.com.br>
2021-01-04 18:34:40 -0300

    create Factories all models

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Client;
+use App\Models\Order;
+use App\Models\Table;
+use App\Models\Tenant;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class OrderFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Order::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'tenant_id' => Tenant::factory(),
+            'client_id' => Client::factory(),
+            'table_id' => Table::factory(),
+            'identify' => uniqid() . Str::random(10),
+            'price' => 80.0,
+            'status' => 'open',
+            'comment' => $this->faker->sentence
+
+        ];
+    }
+}
